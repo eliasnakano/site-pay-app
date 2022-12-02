c96f4f297050aa108c7a68f1da0f5a40c2d98ade
eliasnakano <eliasmoral1237@outlook.com>
2022-12-01 22:36:29 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pay App</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,300;1,400;1,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="shortcut icon" href="./img/favicon.png">
+</head>
+
+<body>
+    <header class="header">
+        <div class="logo">
+            <img src="./img/logo.png">
+        </div>
+        <!--logo-->
+        <ul class="menu-desktop">
+            <li><a href="#"> App </a> </li>
+            <li><a href="#"> Store </a> </li>
+            <li><a href="#"> About Us </a> </li>
+            <li><a href="#"> Contact </a> </li>
+            <button class="btn-menu"><a href="#">Download</a> </button>
+        </ul>
+    </header>
+    <br>
+    <br>
+
+    <section class="texto-phone">
+        <div class="center">
+
+            <div class="w50 texto">
+                <h1>Discover the new way to pay</h1>
+                <p>Lorem ipsum dolor sit amet, conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim a
+                    labore et dolore magnam aliquam quaerat voluptatem.</p>
+                <button class="button">Download</button>
+            </div>
+            <div class="w50 img-phone">
+                <div class="img">
+                    <img src="./img/Smartphone.png">
+                </div>
+
+            </div>
+        </div>
+        <!--center-->
+    </section>
+    <hr>
+
+    <section class="diferenciais">
+        <div class="container cont">
+            <h2>App</h2>
+            <p style="color:white; text-align: center;">Lorem ipsum dolor sit amet, conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim a
+                labore et dolore magnam aliquam quaerat voluptatem</p>
+
+            <div class="icons-diferenciais">
+                <div class="box-single-diferenciais">
+                    <img src="img/img1.png" />
+                    <h3>1-Download the App</h3>
+                    <p>Garanta que toda sua comunicação esteja alinhada com seu propósito</p>
+                </div>
+                <div class="box-single-diferenciais">
+                    <img src="img/img2.png" />
+                    <h3>Install it</h3>
+                    <p>Garanta que toda sua comunicação esteja alinhada com seu propósito </p>
+                </div>
+                <div class="box-single-diferenciais">
+                    <img src="img/img3.png" />
+                    <h3>Create an account</h3>
+                    <p>Garanta que toda sua comunicação esteja alinhada com seu propósito</p>
+                </div>
+                <div class="box-single-diferenciais">
+                    <img src="img/img4.png" />
+                    <h3>Enjoy it</h3>
+                    <p>Garanta que toda sua comunicação esteja alinhada com seu propósito</p>
+                </div>
+
+            </div>
+            <a href="#" class="myButton">Shop Now</a>
+        </div>
+    </section>
+    <hr>
+    <section class="pessoas">
+        <div class="center">
+            <div class="w50 texto-pessoas">
+                <h1>About Us</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it</p>
+                <a href="#" class="myButton">Read More</a>
+            </div>
+
+            <div class="w50 foto-pessoas">
+                <img src="./img/imgpessoas.png">
+            </div>
+
+        </div>
+        <!--center-->
+    </section>
+    <hr>
+
+<section class="pagamento" style="background:#0c223a!important; padding:40px;">
+    <div class="container">
+        <div class="texto-pag">
+        <h1>Our Company</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+        industry's standard dummy text ever since the 1500s</p>
+        </div>
+        <div class="img-pagamento">
+        <img src="./img/pagamento.png">
+        </div>
+      
+    </div>
+
+
+</section>
+
+<hr>
+
+<section class="mapa">
+<div class="container">
+<div class="texto-map">
+<h1>Contact</h1>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+ industry's standard dummy text ever since the 1500s</p>
+</div>
+<div class="img-map">
+    <img src="./img/Map.png">
+    <p>Company Name <br>
+     St Name Street , City<br>
+     Country<br>
+     +00 123 456 789 
+    </p>
+
+</div>
+
+</div><!--container-->
+
+</section>
+
+<hr>
+
+<footer class="footer">
+<div class="center">
+
+<div class="w50 link-fotter">
+    <img src="./img/logo-footer.png">
+    <ul class="footer">
+        <li><a href="#"> App </a></li>
+        <li><a href="#"> Store </a></li>
+        <li><a href="#"> About Us </a></li>
+        <li><a href="#"> Contact </a></li>
+
+    </ul>
+    
+</div>
+
+<div class="fotter-phone">
+    <img src="./img/smartphone2.png">
+
+</div>
+
+
+</div><!--center-->
+<div class="container">
+<div class="social-media">
+<span>Copyright | Elias Nakano</span>
+<i class="fa-brands fa-instagram"><a href="#"></a> </i>
+<i class="fa-brands fa-facebook-f"><a href="#"></a></i>
+<i class="fa-brands fa-twitter"><a href="#"></a></i>
+</div>
+</div>
+</footer>
+
+
+
+  
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,6 @@
+  //menu Humburger codigo js abaixo para abrir
+  $('.menu-mobile i').click(function(){
+    $('.menu-mobile').find('ul').slideToggle();
+});
+
+
